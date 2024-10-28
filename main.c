caf9abb04fc17d588e1e400e5d00fe43332d3eb3
lnicolosi <l.nicolosi8@gmail.com>
2024-10-28 18:07:51 +0100

    0.1

---
+++ main.c
@@ -0,0 +1,19 @@
+#include "philo.h"
+
+/*
+    The Main is a TL;DR of the program
+
+    ./philo 5 800 200 200 [5]
+*/
+int main(int argc, char **argv)
+{
+	if (5 == argc || 6 == argc)
+	{
+		// correct input
+	}
+	else
+	{
+		// wrong input
+	}
+	
+}
---
+++ philo.h
@@ -0,0 +1,95 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <limits.h>
+
+/*
+	ANSI Escape Sequeces for Bold Text Colors
+	Usage :
+		printf(R "This is red text." RST);
+		printf(B "This is blue text." RST);
+	Remember to use RST to reset the color after setting it.
+*/
+
+# define RST	"\033[0m"		/* Reset to default color */
+# define RED	"\033[1;31m"	/* Bold Red */
+# define G		"\033[1;32m"	/* Bold Green */
+# define Y		"\033[1;33m"	/* Bold Yellow */
+# define B		"\033[1;34m"	/* Bold Blue */
+# define M		"\033[1;35m"	/* Bold Magenta */
+# define C		"\033[1;36m"	/* Bold Cyan */
+# define W		"\033[1;37m"	/* Bold White */
+
+
+
+
+//*** structures ***
+
+/*___________________________
+	code more readable
+*/
+typedef pthread_mutex_t	t_mtx;
+
+/*___________________________
+	IOU for compiler
+*/
+typedef struct	s_table	t_table;
+
+/*___________________________
+	FORK
+*/
+typedef struct	s_fork
+{
+	t_mtx	fork;
+	int		fork_id;
+}				t_fork;
+
+/*___________________________
+	PHILO
+
+	./philo 5 800 200 200 [5]
+*/
+typedef struct s_philo
+{
+	int			id;
+	long		meals_counter;
+	bool		full;
+	long		last_meal_time;	// time passed from last meal
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id; // a philo is a thread
+	t_table		*table;
+	
+}				t_philo;
+
+/*_____________________________
+	TABLE
+	
+	./philo 5 800 200 200 [5]
+*/
+struct	s_table
+{
+	long		philo_nbr; // 5
+	long		time_to_die; // 800
+	long		time_to_eat; // 200
+	long		time_to_sleep; // 200
+	long		nbr_limit_meals; // [5] | FLAG if -1
+	long		start_simulation; 
+	bool		end_simulation; // a philo dies or all philo full
+	t_fork		*forks; // array of forks
+	t_philo		*philos; // array of philos
+};
+
+// *** PROTOTYPES ***
+
+// *** utils ***
+void	error_exit(const char *error);
+
+
+#endif
